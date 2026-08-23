f3b3fe201a23c2b12fe101a5c6e76f307a99b47c
fr2685411 <farjanabano1313@gmail.com>
2026-08-23 21:58:15 +0530

    Fix Nebula server container binding

--- nebula-music-server/index.js
+++ nebula-music-server/index.js
@@ -606,7 +606,7 @@ app.get('/health/runtime', async (req, res) => {
 // Start server
 // -----------------------------------------------------------------------------
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(
     `NebulaMusic Server running on port ${PORT}`
   );
